1cd02a3adb9fd62d36a9c6e4be92d8fecd605e05
Time-C <70840511+Time-C@users.noreply.github.com>
2020-09-06 15:34:05 +0530

    Add files via upload

---
+++ project 1a.py
@@ -0,0 +1,6 @@
+r = float(input("What is the Radius of the Circle : "))
+from math import pi
+Area = str(pi*r**2)
+
+print("So finally the AREA of the Circle with the radius" + str(r) + " is --->     " + Area + " units")
+
